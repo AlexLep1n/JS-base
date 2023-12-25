707c6609d32399bef33f1aa44cb9a175cd51fb7e
Alex Lepin <alexlepin@mail.ru>
2023-12-25 15:31:46 +0300

    hw-2 of the advanced js has been completed

--- advanced-js/hw-2/index.js
+++ advanced-js/hw-2/index.js
@@ -68,15 +68,109 @@ const libraryBooks = [
   "Автостопом по галактике",
 ];
 
-// const library = new Library(libraryBooks);
-// console.log(library.books); // books array
-// console.log(library.hasBook("Гарри Поттер и философский камень")); // true
-// console.log(library.hasBook("Гарри Поттер и орден феникса")); // false
-// library.addBook("Гарри Поттер и философский камень"); // catch error and cl error.message
-// library.addBook("Гарри Поттер и тайная комната"); // added
-// console.log(library.books);
-// library.removeBook("Книга, которой нет"); // catch error and cl error.message
-// library.removeBook("Автостопом по галактике");
-// console.log(library.books);
+const library = new Library(libraryBooks);
+console.log(library.books); // books array
+console.log(library.hasBook("Гарри Поттер и философский камень")); // true
+console.log(library.hasBook("Гарри Поттер и орден феникса")); // false
+library.addBook("Гарри Поттер и философский камень"); // catch error and cl error.message
+library.addBook("Гарри Поттер и тайная комната"); // added
+console.log(library.books);
+library.removeBook("Книга, которой нет"); // catch error and cl error.message
+library.removeBook("Автостопом по галактике");
+console.log(library.books);
 
 // Задание №2
+// Вы разрабатываете систему отзывов для вашего веб-сайта. Пользователи могут оставлять отзывы, но чтобы исключить слишком короткие или слишком длинные сообщения, вы решаете установить некоторые ограничения.
+
+// Создайте HTML-структуру с текстовым полем для ввода отзыва, кнопкой для отправки и контейнером, где будут отображаться отзывы.
+
+// Напишите функцию, которая будет добавлять отзыв в контейнер с отзывами. Однако если длина введенного отзыва менее 50 или более 500 символов, функция должна генерировать исключение.
+
+// При добавлении отзыва, он должен отображаться на странице под предыдущими отзывами, а не заменять их.
+
+const initialData = [
+  {
+    product: "Apple iPhone 13",
+    reviews: [
+      {
+        id: "1",
+        text: "Отличный телефон! Батарея держится долго.",
+      },
+      {
+        id: "2",
+        text: "Камера супер, фото выглядят просто потрясающе.",
+      },
+    ],
+  },
+  {
+    product: "Samsung Galaxy Z Fold 3",
+    reviews: [
+      {
+        id: "3",
+        text: "Интересный дизайн, но дорогой.",
+      },
+    ],
+  },
+  {
+    product: "Sony PlayStation 5",
+    reviews: [
+      {
+        id: "4",
+        text: "Люблю играть на PS5, графика на высоте.",
+      },
+    ],
+  },
+];
+
+const textarea = document.querySelector(".feedback__txt");
+const btn = document.querySelector(".feedback__btn");
+const fbList = document.querySelector(".feedback__list");
+fbList.style.fontSize = "24px";
+textarea.style.fontSize = "20px";
+
+addFeedBack(initialData);
+
+btn.addEventListener("click", function () {
+  try {
+    if (textarea.textLength >= 50 && textarea.textLength <= 500) {
+      let divProd = document.createElement("div");
+      let divComment = document.createElement("div");
+      let commentHeading = document.createElement("h3");
+      let commentTxt = document.createElement("p");
+      let initTxt = textarea.value;
+      commentHeading.textContent = initTxt.slice(0, 15);
+      commentTxt.textContent = initTxt.slice(16);
+      divComment.append(commentHeading);
+      divComment.append(commentTxt);
+      divProd.append(divComment);
+      fbList.append(divProd);
+    } else {
+      throw new Error(
+        "Длина отзыва не может быть меньше 50 и больше 500 символов!"
+      );
+    }
+  } catch (error) {
+    alert(error.message);
+  }
+});
+
+function addFeedBack(initialData) {
+  for (const products of initialData) {
+    let divProd = document.createElement("div");
+    let commentHeading = document.createElement("h3");
+    commentHeading.textContent = products.product;
+    divProd.append(commentHeading);
+    let item = products.reviews;
+    for (let i = 0; i < item.length; i++) {
+      let divComment = document.createElement("div");
+      let comment = document.createElement("p");
+      let commentId = document.createElement("p");
+      commentId.textContent = `Комментарий №${item[i].id}`;
+      comment.textContent = item[i].text;
+      divComment.append(commentId);
+      divComment.append(comment);
+      divProd.append(divComment);
+    }
+    fbList.append(divProd);
+  }
+}
